66b2a242e8986c6877a99fbada5114f8a885c6da
Jakob <jakob.h.roenn@gmail.com>
2024-05-09 15:45:18 +0200

    added arrow

--- scripts.js
+++ scripts.js
@@ -6,3 +6,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
